513aa75ec56f95ccc6a9bfdf03cc66e0f172c142
Bench <BinaryBench@gmail.com>
2016-08-31 17:02:32 -0400

    Created working test plugins for Hub & Game.

---
+++ Core/src/main/java/me/binarynetwork/core/BinaryNetworkPlugin.java
@@ -0,0 +1,64 @@
+package me.binarynetwork.core;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * Created by Bench on 8/31/2016.
+ */
+public abstract class BinaryNetworkPlugin extends JavaPlugin {
+
+    private static BinaryNetworkPlugin plugin;
+    public static BinaryNetworkPlugin getPlugin()
+    {
+        return plugin;
+    }
+    public static void registerEvents(Listener listener)
+    {
+        Bukkit.getPluginManager().registerEvents(listener, getPlugin());
+    }
+    public static void unregisterEvents(Listener listener)
+    {
+        HandlerList.unregisterAll(listener);
+    }
+
+
+    private ScheduledExecutorService scheduledExecutorService;
+
+
+
+    @Override
+    public final void onEnable()
+    {
+        plugin = this;
+        scheduledExecutorService = Executors.newScheduledThreadPool(10);
+        enable();
+    }
+
+    @Override
+    public final void onDisable()
+    {
+        disable();
+    }
+
+    public void enable()
+    {
+
+    }
+
+    public void disable()
+    {
+
+    }
+
+
+    public ScheduledExecutorService getScheduledExecutorService()
+    {
+        return scheduledExecutorService;
+    }
+}
---
+++ Hub/src/main/java/me/binarynetwork/hub/Main.java
@@ -0,0 +1,14 @@
+package me.binarynetwork.hub;
+
+import me.binarynetwork.core.BinaryNetworkPlugin;
+
+/**
+ * Created by Bench on 8/31/2016.
+ */
+public class Main extends BinaryNetworkPlugin {
+    @Override
+    public void enable()
+    {
+        System.out.println("This plugin works!");
+    }
+}

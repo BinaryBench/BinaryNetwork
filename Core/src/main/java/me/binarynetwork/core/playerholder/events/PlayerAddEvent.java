1b5752d0387b17b08d1f3d3a39ae0418d0a479d0
Bench <BinaryBench@gmail.com>
2016-09-02 21:31:14 -0400

    Added getPrePlayers and getPostPlayers

--- Core/src/main/java/me/binarynetwork/core/playerholder/events/PlayerAddEvent.java
+++ Core/src/main/java/me/binarynetwork/core/playerholder/events/PlayerAddEvent.java
@@ -1,9 +1,14 @@
 package me.binarynetwork.core.playerholder.events;
 
+import com.comphenix.protocol.PacketType;
 import me.binarynetwork.core.playerholder.PlayerHolder;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by BinaryBench on 3/20/2016.
@@ -25,6 +30,18 @@ public class PlayerAddEvent extends Event {
     public PlayerHolder getPlayerHolder()
     {
         return this.playerHolder;
+    }
+
+    public Collection<Player> getPrePlayers()
+    {
+        return getPlayerHolder().getPlayers();
+    }
+    public Collection<Player> getPostPlayers()
+    {
+        List<Player> players = new ArrayList<>();
+        players.addAll(getPlayerHolder().getPlayers());
+        players.add(getPlayer());
+        return players;
     }
 
     public Player getPlayer()
--- Core/src/main/java/me/binarynetwork/core/playerholder/events/PlayerRemoveEvent.java
+++ Core/src/main/java/me/binarynetwork/core/playerholder/events/PlayerRemoveEvent.java
@@ -5,6 +5,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * Created by BinaryBench on 3/20/2016.
  */
@@ -29,6 +33,20 @@ public class PlayerRemoveEvent extends Event {
     public PlayerHolder getPlayerHolder()
     {
         return this.playerHolder;
+    }
+
+
+    public Collection<Player> getPrePlayers()
+    {
+        return getPlayerHolder().getPlayers();
+    }
+
+    public Collection<Player> getPostPlayers()
+    {
+        List<Player> players = new ArrayList<>();
+        players.addAll(getPlayerHolder().getPlayers());
+        players.remove(getPlayer());
+        return players;
     }
 
     public Player getPlayer()

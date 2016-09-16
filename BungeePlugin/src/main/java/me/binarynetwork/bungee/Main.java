aec468d5e68cad09ff3fc8df894f6e23edfe0bb8
Bench <BinaryBench@gmail.com>
2016-09-16 04:31:50 -0400

    Added BungeePlugin

---
+++ BungeePlugin/src/main/java/me/binarynetwork/bungee/Main.java
@@ -0,0 +1,14 @@
+package me.binarynetwork.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Created by Bench on 9/16/2016.
+ */
+public class Main extends Plugin {
+    @Override
+    public void onEnable()
+    {
+        getLogger().info("Yay! It loads!");
+    }
+}
--- Hub/src/main/java/me/binarynetwork/hub/Main.java
+++ Hub/src/main/java/me/binarynetwork/hub/Main.java
@@ -1,14 +1,8 @@
 package me.binarynetwork.hub;
 
 import me.binarynetwork.core.BinaryNetworkPlugin;
-import me.binarynetwork.core.common.Log;
-import me.binarynetwork.core.common.UUIDFetcher;
-import me.binarynetwork.core.common.utils.ServerUtil;
-import org.apache.logging.log4j.core.helpers.UUIDUtil;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
-
-import java.util.UUID;
 
 /**
  * Created by Bench on 8/31/2016.
